07bcbd648183f9b1bf1c10732cdc63972a875f00
Javier VT <51035830+Javier-VT@users.noreply.github.com>
2024-01-13 22:30:36 -0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,19 @@
+﻿function mostrarMensaje() {
+    var container = document.getElementById('main-container');
+    container.style.opacity = '0';
+
+    setTimeout(function () {
+        container.style.display = 'none';
+
+        var mensajeContainer = document.createElement('div');
+        mensajeContainer.classList.add('container');
+
+        var mensaje = document.createElement('p');
+        mensaje.innerText = "Papá, quiero expresarte cuánto te quiero y aprecio. Eres una persona admirable y mereces lo mejor, gracias por el esfuerzo y dedicación que le has dado a tu familia, tienes mucho por delante, no te rindas. ¡Feliz Cumpleaños!";
+
+        mensajeContainer.appendChild(mensaje);
+        document.body.appendChild(mensajeContainer);
+    }, 500);
+    var cancion = new Audio('../rsc/song.mp3');
+    cancion.play();
+}
